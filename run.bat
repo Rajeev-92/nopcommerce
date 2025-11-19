df5065a311e7b9d9b7613ba66ae045b904758707
Rajeev-92 <122995555+Rajeev-92@users.noreply.github.com>
2025-11-19 08:50:50 +0530

    run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,6 @@
 @echo off
-cd %WORKSPACE%
-
-REM --- Activate the virtual environment correctly ---
 call venv\Scripts\activate
-
-REM --- Run pytest ---
-pytest -s -v -m "sanity" --html reports\test_report.html --browser chrome
-
+pytest -s -v -m "sanity" --html .\reports\test_report_chrome.html --browser chrome
+pytest -s -v -m "regression" --html .\reports\test_report_chrome.html --browser chrome
+pytest -s -v -m "sanity and regression" --html .\reports\test_report_chrome.html --browser chrome
 pause
